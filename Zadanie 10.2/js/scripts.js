cadb0de4b6ab5a12e1cc6b59f974ba96aea828f9
unknown <asiac00@gmail.com>
2017-11-11 14:19:41 +0100

    first commit

---
+++ Zadanie 10.2/js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	
+  var carouselList = $("#carousel ul");
+  
+  setInterval(changesSlide, 4000);
+  
+  function changesSlide(){
+    carouselList.animate({marginLeft:-600}, 500, moveFirstSlide);
+  }
+  
+  function moveFirstSlide(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem)
+    carouselList.css({marginLeft:0});
+  }
+});
+
+/*
+
+https://codepen.io/anon/pen/ZaePoW
+
+https://codepen.io/anon/pen/MOpREv
+
+*/
