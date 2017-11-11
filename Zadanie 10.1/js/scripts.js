b92ec27300c4f01f4a6ec0abe25f6179339c1dc9
unknown <asiac00@gmail.com>
2017-11-11 14:27:47 +0100

    fix task 10.1

--- Zadanie 10.1/js/scripts.js
+++ Zadanie 10.1/js/scripts.js
@@ -20,34 +20,3 @@ paragraphs.each(function(index, element) {
   
 });
 
-/*
-$(function(){
-	console.log('DOM loaded - you can have fun');
-});
-
-var span = $("span");
-span.each(function(index, element) {
-  if (index % 2 == 0) {
-    $(element).css('color', 'blue')
-  }
-});
-
-var colorOfSecondDiv = $("div2");
-colorOfSecondDiv.each(function(index, element) {
-  if (index % 2 == 0) {
-    $(element).css('color', 'green')
-  }
-});
-
-var secondDiv = $("div2");
-secondDiv.each(function(index, element) {
-  var button = '<button class="btn" data-tmp="' + index + '">Click!</button>'
-  $(element).append(button)
-  
-});
-
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
-});
-
-*/
